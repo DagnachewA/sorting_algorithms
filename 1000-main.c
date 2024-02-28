3c25aed035f08bebc73a2b5d9cbe6deeffbcea2d
DagnachewA <wchegnada41@gmail.com>
2024-02-28 12:42:49 -0800

    Sorting algorithms & Big O

--- 1000-main.c
+++ 1000-main.c
@@ -22,25 +22,25 @@ void print_deck(const deck_node_t *deck)
 
 deck_node_t *init_deck(const card_t cards[52])
 {
-    deck_node_t *deck;
-    deck_node_t *node;
-    size_t i;
-
-    i = 52;
-    deck = NULL;
-    while (i--)
-    {
-        node = malloc(sizeof(*node));
-        if (!node)
-            return (NULL);
-        node->card = &cards[i];
-        node->next = deck;
-        node->prev = NULL;
-        if (deck)
-            deck->prev = node;
-        deck = node;
-    }
-    return (deck);
+	deck_node_t *deck;
+	deck_node_t *node;
+	size_t i;
+	
+	i = 52;
+	deck = NULL;
+	while (i--)
+	{
+	    node = malloc(sizeof(*node));
+	    if (!node)
+		    return (NULL);
+	    node->card = &cards[i];
+	    node->next = deck;
+	    node->prev = NULL;
+	    if (deck)
+		    deck->prev = node;
+	    deck = node;
+	}
+	return (deck);
 }
 
 int main(void)
