cfab169be379f30824aff118a79d5b03f62174e9
DagnachewA <wchegnada41@gmail.com>
2024-02-28 11:59:16 -0800

    Big O

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,12 +9,12 @@
 void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 {
 	(*n1)->next = n2->next;
-  
+	
 	if (n2->next != NULL)
 		n2->next->prev = *n1;
 	n2->prev = (*n1)->prev;
 	n2->next = *n1;
-  
+
 	if ((*n1)->prev != NULL)
 		(*n1)->prev->next = n2;
 	else
@@ -40,7 +40,7 @@ void insertion_sort_list(listint_t **list)
 	{
 		tmp = iter->next;
 		insert = iter->prev;
-    
+
 		while (insert != NULL && iter->n < insert->n)
 		{
 			swap_nodes(list, &insert, iter);
--- 1-main.c
+++ 1-main.c
@@ -18,17 +18,17 @@ listint_t *create_listint(const int *array, size_t size)
 
     list = NULL;
     while (size--)
-    {
-        node = malloc(sizeof(*node));
-        if (!node)
-            return (NULL);
-        tmp = (int *)&node->n;
-        *tmp = array[size];
-        node->next = list;
-        node->prev = NULL;
-        list = node;
-        if (list->next)
-            list->next->prev = list;
+    {   
+	    node = malloc(sizeof(*node));
+	    if (!node)
+		    return (NULL);
+	    tmp = (int *)&node->n;
+	    *tmp = array[size];
+	    node->next = list;
+	    node->prev = NULL;
+	    list = node;
+	    if (list->next)
+		    list->next->prev = list;
     }
     return (list);
 }
@@ -43,10 +43,10 @@ int main(void)
     listint_t *list;
     int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
     size_t n = sizeof(array) / sizeof(array[0]);
-
+    
     list = create_listint(array, n);
     if (!list)
-        return (1);
+	    return (1);
     print_list(list);
     printf("\n");
     insertion_sort_list(&list);
